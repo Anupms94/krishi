6513a888342d18f0f9236366702b7a12e8b443f6
Anupms94 <69163461+Anupms94@users.noreply.github.com>
2022-04-20 14:32:53 +0530

    Update home.php

--- secondpage/customer/home.php
+++ secondpage/customer/home.php
@@ -185,7 +185,7 @@ if(!isset($_SESSION["sess_user"])){
             <h2 class="title">Latest upload</h2>
             <div class="row">
                 <?php
-         $conn = new PDO("sqlsrv:server = tcp:krishiapp.database.windows.net,1433; Database = examplevalid", "Anup98", "Gandhi98@");
+         $conn = mysqli_connect("tcp:krishiapp.database.windows.net,1433", "Anup98", "Gandhi98@", "examplevalid");
           //$conn = mysqli_connect("localhost", "root", "", "examplevalid");
         //check connection
            if(!$conn){
